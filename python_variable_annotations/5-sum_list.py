96a719a84ee4b409b15f28d23ee634ef4b418fc2
JBMjese <6935@holbertonstudents.com>
2024-05-06 17:57:19 -0500

    :heavy_plus_sign: Point 5 funtion summ_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Add all elements of a list of floating point numbers"""
+
+
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ input_list of floats as argument
+    and returns their sum as a floatt """
+    return sum(input_list)
