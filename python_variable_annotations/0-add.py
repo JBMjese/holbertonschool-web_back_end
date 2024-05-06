c55b006f9ecc0a201268c8a11ae68ad469a5fd79
JBMjese <6935@holbertonstudents.com>
2024-05-06 16:50:31 -0500

    :alien: Added type annotations and validation to function add for floats Point 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Adds two floating point numbers and returns the result."""
+
+def add(a: float, b: float) -> float:
+    """function add that takes a float
+        a and a float b as arguments and
+        returns their sum as a float """
+    return a + b
