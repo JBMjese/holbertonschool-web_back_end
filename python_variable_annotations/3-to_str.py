68df6fec355177fbf33e32179ff18f6ee90bcd06
JBMjese <6935@holbertonstudents.com>
2024-05-08 10:18:59 -0500

    fix point 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Converts a floating point number to a string."""
+
+
 def to_str(n: float) -> str:
     """ n as argument and returns the string
         epresentation of the float """
