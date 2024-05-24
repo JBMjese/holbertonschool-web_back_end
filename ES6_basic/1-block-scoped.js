260de4cfad4265276801e65f8fd4fdb59cfcfab5
JBMjese <6935@holbertonstudents.com>
2024-05-23 20:54:00 -0500

    :ambulance: Fix Point 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const inertask = true;
-    const inertask2 = false;
+    return [task, task2];
   }
 
   return [task, task2];
